2c31d4e2e1c272798fb8bb90bf788b1412800874
Valmor Júnior <valmor@valmor.com>
2024-08-14 23:19:47 -0300

    configuration views

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -4,4 +4,4 @@ from .models import Message
 
 def home(request):
     messages = Message.objects.all()
-    return render(request, "index.html", {messages})
+    return render(request, "index.html", {"messages": messages})
